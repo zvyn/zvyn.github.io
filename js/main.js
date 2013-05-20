d25cf66dc65385587529407c9d15fb5cceb259d5
Milan Oberkirch <oberkirm@informatik.uni-freiburg.de>
2013-05-20 13:08:21 +0200

    Let jekyll generate full static html sites and ajax-snippets from _includes.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+function setLinks() {
+  $(".link-impressum").attr('href', 'javascript: loadToMainDiv("impressum.html", "Impressum")');
+  $(".link-home").attr('href', 'javascript: loadToMainDiv("home.html", "Milan Oberkirch");');
+}
+
+function loadToMainDiv(href, title) {
+  var div = $("#main-content");
+  div.fadeOut("fast", function() {
+  document.title = title;
+  div.load(href, function () {
+    setLinks();
+    div.fadeIn("fast");}
+          );
+  });
+}
+
 $(document).ready(function(){
+  setLinks();
   $("#main-content").fadeIn("slow");
 })
