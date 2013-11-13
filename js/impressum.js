e70db2459c0152115ed8873e7d066f2ec2a24ff4
Milan Oberkirch <milan@oberkirch.org>
2013-11-13 15:41:59 +0100

    impressum

--- js/impressum.js
+++ js/impressum.js
@@ -4,7 +4,7 @@
     // ==========================
 
     $(function () {
-        var content_object = $('#impressum_content').hide()
+        var content_object = $('#impressum_content').toggle(slow)
         $('#impressum_link').on('click', function(){
             content_object.toggle('slow');
         });
