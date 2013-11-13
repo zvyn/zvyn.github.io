7dfc5e5f533817eac938537c2212972e2ec4df03
Milan Oberkirch <milan@oberkirch.org>
2013-11-13 15:40:53 +0100

    impressum ipressing

--- js/impressum.js
+++ js/impressum.js
@@ -4,19 +4,9 @@
     // ==========================
 
     $(function () {
-        var content_object = $('#impressum_content').hide()[0]
-        var popover = $('#impressum_link').popover({
-            toggle: 'popover',
-            placement: 'bottom',
-            html: true,
-            title: 'Impressum <a href="#" id="impressum_close" ' +
-                   'class="btn btn-default glyphicon glyphicon-remove"> </a>',
-            content: content_object.innerHTML,
-        }).on('shown.bs.popover', function() {
-            $("#impressum_close").on('click', function(){
-                popover.popover('hide')
-                console.log("hans\n")
-            })
-        })
-    })
+        var content_object = $('#impressum_content').hide()
+        $('#impressum_link').on('click', function(){
+            content_object.toggle('slow');
+        });
+    });
 }(jQuery);
