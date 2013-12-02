e18327a6b9462ffa0e6bd8597396b9a4ecffeea3
Milan Oberkirch <milan@oberkirch.org>
2013-12-02 04:02:01 +0100

    some js-finetuning

--- js/impressum.js
+++ js/impressum.js
@@ -4,7 +4,8 @@
     // ==========================
 
     $(function () {
-        var content_object = $('#impressum_content').hide();
+        var content_object = $('#impressum_content').hide(
+            function(){$("body").fadeIn()});
         $('#impressum_link').on('click', function(){
             content_object.toggle('slow');
         });
